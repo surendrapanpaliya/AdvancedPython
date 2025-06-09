82be1aa819e222ad7ba9c23357f8fcaba82c641b
Surendra R Panpaliya <108717836+surendrapanpaliya@users.noreply.github.com>
2025-06-09 18:23:29 +0530

    Add files via upload
    Day1: Hands-on Session.

---
+++ greatest_of_three.py
@@ -0,0 +1,15 @@
+
+# Control Structure
+# if elif else
+# greatest of three numbers
+
+first = int(input("Enter first:"))
+second = int(input("Enter Second:"))
+third = int(input("Enter third:"))
+
+if first > second and first > third :
+    print("First is greater",first)
+elif second > third:
+    print("second is greater",second)
+else:
+    print("Third is greater",third)
